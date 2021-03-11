965aab4e44a1d656f40ca42e192791c84a8ae4ad
El Strategico <strategico.dev@gmail.com>
2021-03-11 15:29:30 +0800

    Update

--- main.cpp
+++ main.cpp
@@ -2,17 +2,30 @@
 
 #include <Environment/Stack.hpp>
 #include <Parsers/LexicalParser.hpp>
+#include <Environment/Variables.hpp>
 
 int main()
 {
-	std::string command = "(+ 1 (+ (+ 1 1) (+ 1 1)))";
+	std::string commandVar = "(define x 1)";
+	std::string commandPlus = "(+ x x)";
+
 	LexicalParser lexicalParser;
 
-	Expression* exp = new Expression();
-	lexicalParser.parseCommand(command, exp);
-	exp->execute();
+	Expression* expVar = new Expression();
+	Expression* expPlus = new Expression();
 
+	lexicalParser.parseCommand(commandVar, expVar);
+	expVar->execute();
+
+	lexicalParser.parseCommand(commandPlus, expPlus);
+	expPlus->execute();
+	
 	std::cout << Stack::pop() << std::endl;
+
+	Variables::free();
+
+	delete expVar;
+	delete expPlus;
 
 	return 0;
 }
